ecbb620d3925e3e4b4f9ab2a7fa1464c8a8f1eda
Ike Celestine <idcelestine@yahoo.com>
2025-11-27 15:29:27 +0000

    Fix AppRoutes import path

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import AppRoutes from "./Routes/AppRoutes";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
